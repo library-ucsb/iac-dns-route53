376f25e21e51da7a779faa782f2cf175d16ad606
Josh Preston <joshuapreston@ucsb.edu>
2022-07-14 14:55:05 -0700

    added TFE to the providers listing

--- terraform/zones/providers.tf
+++ terraform/zones/providers.tf
@@ -4,9 +4,18 @@ terraform {
       source = "integrations/github"
       version = "4.26.1"
     }
+    tfe = {
+      source = "hashicorp/tfe"
+      version = "0.33.0"
+    }
   }
 }
 
 provider "github" {
   owner = var.github_owner
 }
+
+provider "tfe" {
+  # Configuration options
+}
+
