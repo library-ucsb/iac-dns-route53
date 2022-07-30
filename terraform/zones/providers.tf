3f41c95d0cd48001e1cb6ca71edd5126480b201b
Josh Preston <joshuapreston@ucsb.edu>
2022-07-29 21:17:17 -0700

    unpinned the provider versions to resolve error importing

--- terraform/zones/providers.tf
+++ terraform/zones/providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     github = {
       source = "integrations/github"
-      version = "4.26.1"
+      version = "~>4.26.1"
     }
     aws = {
       source = "hashicorp/aws"
-      version = "4.22.0"
+      version = "~>4.22.0"
     }
   }
 }
