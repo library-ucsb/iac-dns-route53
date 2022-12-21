46f36dd5c12ebf646a3c1a132f419b095ea95d4d
Michael Craig <mcraig>
2022-12-21 15:36:27 -0700

    restored original record values

--- terraform/zones/zone.alexandria.ucsb.edu.tf
+++ terraform/zones/zone.alexandria.ucsb.edu.tf
@@ -7,7 +7,7 @@ resource "aws_route53_record" "www-alexandria-ucsb-edu-CNAME" {
   name    = "www.alexandria.ucsb.edu."
   type    = "CNAME"
   ttl     = "10800"
-  records = ["haproxyt1.library.ucsb.edu."]
+  records = ["haproxyt.library.ucsb.edu."]
 }
 
 resource "aws_route53_record" "ucftp-alexandria-ucsb-edu-CNAME" {
@@ -64,7 +64,7 @@ resource "aws_route53_record" "alexandria-ucsb-edu-A" {
   name    = "alexandria.ucsb.edu."
   type    = "A"
   ttl     = "10800"
-  records = ["128.111.87.13"]
+  records = ["128.111.87.12"]
 }
 
 #  All *.legacy.library.ucsb.edu requests
