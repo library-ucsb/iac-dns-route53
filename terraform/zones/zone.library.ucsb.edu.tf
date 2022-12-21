e3ebe20afc5b589803d8dfaa7b3ef3f6180857f3
Michael Craig <mcraig>
2022-12-21 15:33:39 -0700

    restored ttl value

--- terraform/zones/zone.library.ucsb.edu.tf
+++ terraform/zones/zone.library.ucsb.edu.tf
@@ -730,7 +730,7 @@ resource "aws_route53_record" "haproxyt-library-ucsb-edu-A" {
 zone_id = local.library-zone_id
   name    = "haproxyt.library.ucsb.edu."
   type    = "A"
-  ttl     = "30"
+  ttl     = "10800"
   records = ["128.111.87.12"]
 }
 
