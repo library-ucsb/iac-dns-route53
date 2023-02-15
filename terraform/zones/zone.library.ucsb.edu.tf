746ad3fd61c360b83b5ca05c904d7cbd1f9e38e4
michael-cr41g <82611274+michael-cr41g@users.noreply.github.com>
2023-02-14 18:11:34 -0700

    remove record from testing
    this served its purpose

--- terraform/zones/zone.library.ucsb.edu.tf
+++ terraform/zones/zone.library.ucsb.edu.tf
@@ -38,14 +38,6 @@ zone_id = local.library-zone_id
   records = ["dfxvhm9lq0zwe.cloudfront.net."]
 }
 
-resource "aws_route53_record" "we-remember-them-test-library-ucsb-edu-CNAME" {
-zone_id = local.library-zone_id
-  name    = "we-remember-them-test.library.ucsb.edu."
-  type    = "CNAME"
-  ttl     = "20"
-  records = ["dfxvhm9lq0zwe.cloudfront.net."]
-}
-
 resource "aws_route53_record" "vpn-library-ucsb-edu-CNAME" {
 zone_id = local.library-zone_id
   name    = "vpn.library.ucsb.edu."
