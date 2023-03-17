e5bb2c15e578130b8cdd25fabf2d5fce20c94010
Michael Craig <mscraig@ucsb.edu>
2023-03-17 11:05:20 -0700

    add PTR for managefilemaker

--- terraform/zones/zone.97.111.128.in-addr.arpa.tf
+++ terraform/zones/zone.97.111.128.in-addr.arpa.tf
@@ -1,3 +1,11 @@
 locals {
   rev97-zone_id = aws_route53_zone.r53_zones["reverse-97-111-128-in-addr-arpa"].zone_id
 }
+
+resource "aws_route53_record" "_222-97-111-128-in-addr-arpa-PTR" {
+  zone_id = local.rev97-zone_id
+  name    = "222.97.111.128.in-addr.arpa."
+  type    = "PTR"
+  ttl     = "10800"
+  records = ["managefilemaker.library.ucsb.edu."]
+}
