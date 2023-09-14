be1bfa258f349b5924c85a8885d71fc4c9128d6e
michael-cr41g <82611274+michael-cr41g@users.noreply.github.com>
2023-09-14 11:07:38 -0600

    reduce ttl on ezproxy A record

--- terraform/zones/zone.library.ucsb.edu.tf
+++ terraform/zones/zone.library.ucsb.edu.tf
@@ -352,7 +352,7 @@ resource "aws_route53_record" "proxy-library-ucsb-edu-A" {
 zone_id = local.library-zone_id
   name    = "proxy.library.ucsb.edu."
   type    = "A"
-  ttl     = "10800"
+  ttl     = "300"
   records = ["128.111.121.42"]
 }
 
