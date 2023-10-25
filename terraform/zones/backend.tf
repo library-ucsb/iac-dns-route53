f611b07ea538d8ca99f9266512bccd12a5a7135d
michael-cr41g <82611274+michael-cr41g@users.noreply.github.com>
2023-10-25 14:33:30 -0600

    add cnames for sendgrid domain verification (#99)
    * add cnames for sendgrid domain verification
    
    associated with aeon service
    
    * update deprecated parameter
    
    * allow workflow to run other than when submitting approval
    
    * fix data structure
    
    * remove assume_role since it's empty

--- terraform/zones/backend.tf
+++ terraform/zones/backend.tf
@@ -2,12 +2,11 @@ terraform {
   required_version = ">= 0.12.2"
 
   backend "s3" {
-    region         = "us-west-2"
-    bucket         = "ucsb-library-iac-terraform-zones-state"
-    key            = "terraform.tfstate"
-    dynamodb_table = "ucsb-library-iac-terraform-zones-state-lock"
-    profile        = ""
-    role_arn       = ""
-    encrypt        = "true"
+    region          = "us-west-2"
+    bucket          = "ucsb-library-iac-terraform-zones-state"
+    key             = "terraform.tfstate"
+    dynamodb_table  = "ucsb-library-iac-terraform-zones-state-lock"
+    profile         = ""
+    encrypt         = "true"
   }
 }
