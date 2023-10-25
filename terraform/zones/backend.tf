b5ec2fdbe11489faa30b4d84b9f15e25919bb4c0
michael-cr41g <82611274+michael-cr41g@users.noreply.github.com>
2023-10-25 12:43:59 -0600

    fix data structure

--- terraform/zones/backend.tf
+++ terraform/zones/backend.tf
@@ -2,12 +2,14 @@ terraform {
   required_version = ">= 0.12.2"
 
   backend "s3" {
-    region                = "us-west-2"
-    bucket                = "ucsb-library-iac-terraform-zones-state"
-    key                   = "terraform.tfstate"
-    dynamodb_table        = "ucsb-library-iac-terraform-zones-state-lock"
-    profile               = ""
-    assume_role.role_arn  = ""
-    encrypt               = "true"
+    region          = "us-west-2"
+    bucket          = "ucsb-library-iac-terraform-zones-state"
+    key             = "terraform.tfstate"
+    dynamodb_table  = "ucsb-library-iac-terraform-zones-state-lock"
+    profile         = ""
+    assume_role {
+      role_arn      = ""
+    }
+    encrypt         = "true"
   }
 }
