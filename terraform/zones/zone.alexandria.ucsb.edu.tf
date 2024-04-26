b1596dc99557b69602b092e0140884f25f630b18
Michael Craig <mscraig@ucsb.edu>
2024-04-26 10:00:46 -0700

    create broken piru record for testing

--- terraform/zones/zone.alexandria.ucsb.edu.tf
+++ terraform/zones/zone.alexandria.ucsb.edu.tf
@@ -18,6 +18,17 @@ resource "aws_route53_record" "ucftp-alexandria-ucsb-edu-CNAME" {
   records = ["ftpd-352.library.ucsb.edu."]
 }
 
+resource "aws_route53_record" "pirutest-alexandria-ucsb-edu-A" {
+zone_id = local.library-zone_id
+  name    = "pirutest.alexandria.ucsb.edu."
+  type    = "A"
+  alias {
+    name                   = data.aws_lb.dld-eks-ingress-nginx-v3.dns_name
+    zone_id                = data.aws_lb.dld-eks-ingress-nginx-v3.zone_id
+    evaluate_target_health = true
+  }
+}
+
 resource "aws_route53_record" "piru-alexandria-ucsb-edu-CNAME" {
   zone_id = local.alex-zone_id
   name    = "piru.alexandria.ucsb.edu."
